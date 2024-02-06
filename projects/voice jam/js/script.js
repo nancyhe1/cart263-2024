e17b834908d91513fdf0b65c607917fd72938bf3
nancyhe1 <nancyhe098@gmail.com>
2024-02-06 15:05:15 -0500

    voice jam: putting states

--- projects/voice jam/js/script.js
+++ projects/voice jam/js/script.js
@@ -6,7 +6,68 @@
  */
 
 "use strict";
-const speechRecognizer = new p5.SpeechRecognizer(`zh-CN`);
+
+let state = `title`; // can be title, simulation
+
+const speechRecognizer = new p5.SpeechRec(`zh-CN`);
+const speechSynthesizer = new p5.Speech();
+const displayTextMandarin = `你好`;
+const displayTextPhonetics = `nǐ hǎo`;
+
+function setup() {
+    createCanvas(500, 500);
+
+    speechRecognizer.onResult = handleSpeechInput;
+    speechRecognizer.continuous = true;
+    speechRecognizer.start();
+}
+
+function draw() {
+
+    if (state === 'title') {
+        title();
+      }
+      else if (state === 'simulation') {
+        simulation();
+      }
+  
+}
+
+function handleSpeechInput() {
+    if (!speechRecognizer.resultValue) {
+        return;
+    }
+    console.log(speechRecognizer.resultString);
+
+}
+
+function title() {
+    background(240, 207, 255);
+
+    textSize(30);
+    fill(255);
+    textAlign(CENTER,CENTER);
+    text(`Let's see how good is your Chinese pronounciation`, width/2, height/2);
+  
+}
+function simulation() {
+    background(0);
+  
+    textSize(64);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  
+  push();
+  if (speechRecognizer.resultString === `你好`) {
+    fill(50, 255, 50);
+  }
+  text(displayTextMandarin, width/2, height/3);
+  pop();
+  
+  text(displayTextPhonetics, width/2, 2 * height/3);
+}
+
+/* const speechRecognizer = new p5.SpeechRecognizer(`zh-CN`);
 const speechSynthesizer = new p5.Speech();
 const MandarinText = [
     "去",
@@ -41,4 +102,4 @@ function handleSpeechInput() {
     }
     console.log(speechRecognizer.resultString);
 
-}
+} */

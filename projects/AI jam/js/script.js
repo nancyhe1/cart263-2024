67d9b0191e1d126abf8cf851e4cd092f808dbf99
nancyhe1 <nancyhe098@gmail.com>
2024-02-23 01:25:23 -0500

    started Ai jam

---
+++ projects/AI jam/js/script.js
@@ -0,0 +1,33 @@
+/**
+AI Jam
+Nancy He
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
